183476b2216d3554f0dd549c05a82df18f80c3b1
jgkwak <devkkwak@gmail.com>
2023-01-26 11:23:14 +0900

    Initial commit

---
+++ src/main/resources/static/home/resources/scripts/index.js
@@ -0,0 +1,82 @@
+var TxtRotate = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + "</span>";
+
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+
+    if (this.isDeleting) {
+        delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName("txt-rotate");
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute("data-rotate");
+        var period = elements[i].getAttribute("data-period");
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+};
+
+
+
+function check() {
+    let check = true;
+    window.document.getElementById("contactContainer").action = "https://script.google.com/macros/s/AKfycbxqA3J28aAqiLv6heGLli7dvuXF5eOTFF05amh_hRiAb_viAgZDKoyaRTjcR3ef0w/exec";
+    const email = window.document.getElementById("email").value;
+    if (email === ''){
+        check = false;
+    }
+    if (email !== "") {
+        const exptext = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+        if (exptext.test(email) === false) {
+            //이메일 형식이 알파벳+숫자@알파벳+숫자.알파벳+숫자 형식이 아닐경우
+            alert("입력한 메일형식이 올바르지 않습니다.");
+            email.focus();
+            window.document.getElementById("contactContainer").action = " ";
+            check = false;
+        }
+    }
+    if (check === true) {
+        alert('메일이 전송되었습니다.');
+    }
+}
